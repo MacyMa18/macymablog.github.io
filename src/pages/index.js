80ca5d2b91d9e9fe6eb5702a6c25921e25afd4cf
MacyMa18 <macyma18@gmail.com>
2019-06-18 17:45:37 +1200

    add background and delete some unuse files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Link } from "gatsby";
+import styled from "styled-components";
 
-import Layout from "../components/layout";
-import Image from "../components/image";
+import "./styles.css";
 import SEO from "../components/seo";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <SEO title="MacyMa Blog" />
+  </>
 );
 
 export default IndexPage;
