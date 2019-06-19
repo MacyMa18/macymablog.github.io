bb7d9753dba1fa87504af70e24dc7898757bff71
MacyMa18 <macyma18@gmail.com>
2019-06-19 18:08:04 +1200

    add CNAME

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,19 @@ const HomeWrap = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
+  display: flex;
+  align-items: center;
   background: linear-gradient(
     to top,
     rgba(1, 50, 31, 1) 15%,
     rgb(255, 255, 255, 0.1) 100%
   );
+`;
+
+const Name = styled.div`
+  text-align: center;
+  color: #fff;
+  margin: auto;
 `;
 
 const Navigation = styled.div``;
@@ -24,6 +32,7 @@ const SocialItem = styled.div``;
 const IndexPage = () => (
   <HomeWrap>
     <SEO title="MacyMa Blog" />
+    <Name>Macy Ma</Name>
     <Navigation></Navigation>
   </HomeWrap>
 );
