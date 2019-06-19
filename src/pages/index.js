1af38668bb20c0cfa20b026e87cbd2cf8317e6a4
MacyMa18 <macyma18@gmail.com>
2019-06-19 17:10:13 +1200

    background gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,28 @@ import styled from "styled-components";
 import "./styles.css";
 import SEO from "../components/seo";
 
+const HomeWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(
+    to top,
+    rgba(1, 50, 31, 1) 15%,
+    rgb(255, 255, 255, 0.1) 100%
+  );
+`;
+
+const Navigation = styled.div``;
+
+const Item = styled.div``;
+
+const SocialItem = styled.div``;
+
 const IndexPage = () => (
-  <>
+  <HomeWrap>
     <SEO title="MacyMa Blog" />
-  </>
+    <Navigation></Navigation>
+  </HomeWrap>
 );
 
 export default IndexPage;
