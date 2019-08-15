63d88716fbcb51d357f4b8ba6eb83d67787fda91
MacyMa18 <macyma18@gmail.com>
2019-08-15 21:26:39 +1200

    change design

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,12 +5,9 @@ import './menu.scss';
 function Menu() {
   return (
     <div className="menu-wrapper">
-      <div className="blog-title">Macy Ma</div>
-      <div className="menu-item-wrapper">
-        <div className="menu-item">Photography</div>
-        <div className="menu-item">About Me</div>
-        <div className="menu-item">CV</div>
-      </div>
+      <div className="menu-item">Photography</div>
+      <div className="menu-item">About Me</div>
+      <div className="menu-item">CV</div>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,11 @@ import './index.scss';
 function IndexPage() {
   return (
     <div className="homepage-wrapper">
-      <SEO title="MacyMa Blog" />
-      <Menu />
-      <div className="homepage-main-section">
-        <img
-          className="homepage-main-image"
-          src="https://assets.sapi.work/apps/Macy-Blog/IMG_3413.jpg"
-          alt="macyma-blog-img"
-        />
+      <div className="homepage-main-wrapper">
+        <SEO title="MacyMa Blog" />
+        <div className="blog-title">Macy Ma</div>
+        <Menu />
       </div>
-      <Footer />
     </div>
   );
 }
